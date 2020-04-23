86b45f2f83b9eec02a111587fbd1234c9e6fc674
LeonAdeoye <leon.adeoye@gmail.com>
2020-04-23 22:53:25 +0800

    added more unit testing

--- src/test/java/com/leon/services/UserServiceTest.java
+++ src/test/java/com/leon/services/UserServiceTest.java
@@ -29,10 +29,12 @@ public class UserServiceTest
     private UserService userService;
 
     @Test
-    public void whenPassedValidConfiguration_saveConfiguration_shouldCallSaveMethodInRepositoryMock()
+    public void whenPassedValidUsage_saveUsage_shouldCallSaveMethodInRepositoryMock()
     {
         // Arrange
-        Usage usage = new Usage("users app", "horatio", "get configurations", new ArrayList<>(Collections.nCopies(12, OptionalInt.of(0))));
+        List<OptionalInt> monthlyCounts = new ArrayList<>(Collections.nCopies(12, OptionalInt.of(0)));
+        monthlyCounts.set(userService.getCurrentMonthIndex(), OptionalInt.of(1));
+        Usage usage = new Usage("users app", "horatio", "get configurations", monthlyCounts);
         // Act
         userService.saveUsage("users app", "horatio", "get configurations");
         // Assert
@@ -40,7 +42,47 @@ public class UserServiceTest
     }
 
     @Test
-    public void whenPassedValidConfiguration()
+    public void whenPassedValidUsageForExistingAppButDifferentUser_saveUsage_shouldCallSaveMethodInRepositoryMock()
+    {
+        // Arrange
+        List<Usage> usages = Arrays.asList(
+                new Usage("users app", "horatio", "get configuration", new ArrayList<>(Collections.nCopies(12, OptionalInt.of(10)))),
+                new Usage("users app", "harper", "get configuration", new ArrayList<>(Collections.nCopies(12, OptionalInt.of(1)))));
+
+        when(userRepositoryMock.findAll(new Sort(Sort.Direction.ASC, "app"))).thenReturn(usages);
+        userService.initialize();
+        List<OptionalInt> monthlyCounts = new ArrayList<>(Collections.nCopies(12, OptionalInt.of(0)));
+        monthlyCounts.set(userService.getCurrentMonthIndex(), OptionalInt.of(1));
+
+        Usage usage = new Usage("users app", "saori", "get configuration", monthlyCounts);
+        // Act
+        userService.saveUsage("users app", "saori", "get configuration");
+        // Assert
+        verify(userRepositoryMock, times(1)).save(usage);
+    }
+
+    @Test
+    public void whenPassedValidUsageForNonExistentApp_saveUsage_shouldCallSaveMethodInRepositoryMock()
+    {
+        // Arrange
+        List<Usage> usages = Arrays.asList(
+                new Usage("users app", "horatio", "get configuration", new ArrayList<>(Collections.nCopies(12, OptionalInt.of(10)))),
+                new Usage("users app", "harper", "get configuration", new ArrayList<>(Collections.nCopies(12, OptionalInt.of(1)))));
+
+        when(userRepositoryMock.findAll(new Sort(Sort.Direction.ASC, "app"))).thenReturn(usages);
+        userService.initialize();
+        List<OptionalInt> monthlyCounts = new ArrayList<>(Collections.nCopies(12, OptionalInt.of(0)));
+        monthlyCounts.set(userService.getCurrentMonthIndex(), OptionalInt.of(1));
+
+        Usage usage = new Usage("config app", "saori", "get configuration", monthlyCounts);
+        // Act
+        userService.saveUsage("config app", "saori", "get configuration");
+        // Assert
+        verify(userRepositoryMock, times(1)).save(usage);
+    }
+
+    @Test
+    public void whenPassedValidUsageForExistingAppAndExistingUser_saveUsage_shouldCallSaveMethodInRepositoryMock()
     {
         // Arrange
         List<Usage> usages = Arrays.asList(
