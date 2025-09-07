4edcd9405a8cd9000e9192e7ef046f1992ab1629
LeonAdeoye <leon.adeoye@gmail.com>
2025-09-07 14:22:34 +0800

    Upgraded

---
+++ src/main/java/com/leon/controllers/HealthController.java
@@ -0,0 +1,17 @@
+package com.leon.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class HealthController
+{
+    @GetMapping("/health")
+    public ResponseEntity<String> health()
+    {
+        return ResponseEntity.ok("Up");
+    }
+}
